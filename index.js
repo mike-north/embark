13c3bad6163fe3dc4016192b6e4650a6fd0dfaa4
Mike North <michael.l.north@gmail.com>
2017-03-01 22:43:18 -0800

    Filter out null broccoli trees

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ module.exports = {
         tree,
         stew.mv(path.join(path.dirname(require.resolve('jquery-sparkline'))), 'jquery-sparkline' ),
         stew.mv(bootstrapAssetPath('js/dist'), 'bootstrap')
-      ]),
+      ].map(Boolean)),
       { name: 'vennndor' }
     );
   },
